0c71423ef83d75f8e6256f08aac053718eb5e1c1
Jenill-CG <jenil@convegenius.ai>
2024-11-14 19:08:35 +0530

    Update 141124.py

--- 141124.py
+++ 141124.py
@@ -745,7 +745,7 @@ def main():
                 'SESSION': 12
             }
         else:
-            column_names = ['S.NO', 'STUDENT ID', 'STUDENT NAME', 'GENDER', 'AGE', 'SUBJECT 1', 'SUBJECT 2']
+            column_names = ['STUDENT ID', 'STUDENT NAME', 'GENDER', 'AGE', 'SUBJECT 1', 'SUBJECT 2']
             column_widths = {
                 'S.NO': 6,
                 'STUDENT ID': 15,
